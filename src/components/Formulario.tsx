a656bbe5f85e1190e9c133ee2bf6cf76cf5036e7
Brenno <brennofruhauf@outlook.com>
2024-06-12 02:21:09 -0300

    Finish form

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,11 +1,23 @@
 "use client"
 
+import { useState } from "react"
+
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
+import sendData from "@/actions/action"
 import { zodResolver } from "@hookform/resolvers/zod"
 import InputMask from "@mona-health/react-input-mask"
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "./ui/alert-dialog"
 import { Button } from "./ui/button"
 import {
   Form,
@@ -18,17 +30,20 @@ import {
 import { Input } from "./ui/input"
 
 export const Formulario = () => {
+  const [showDialog, setShowDialog] = useState(false)
+  const [loading, setLoading] = useState(false)
+
   const formSchema = z.object({
-    nome: z
+    name: z
       .string()
       .min(3, { message: "Seu nome precisa ter mais de 3 letras" }),
-    telefone: z.string().regex(/^\(\d{2}\) \d \d{4} \d{4}$/, {
+    number: z.string().regex(/^\(\d{2}\) \d \d{4} \d{4}$/, {
       message: "Seu telefone é inválido",
     }),
-    nomeMozao: z
+    baeName: z
       .string()
       .min(3, { message: "O nome do mozão precisa ter mais de 3 letras" }),
-    telefoneMozao: z.string().regex(/^\(\d{2}\) \d \d{4} \d{4}$/, {
+    baeNumber: z.string().regex(/^\(\d{2}\) \d \d{4} \d{4}$/, {
       message: "Telefone do mozão é inválido",
     }),
   })
@@ -36,87 +51,122 @@ export const Formulario = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      nome: "",
-      telefone: "",
-      nomeMozao: "",
-      telefoneMozao: "",
+      name: "",
+      number: "",
+      baeName: "",
+      baeNumber: "",
     },
   })
 
-  const onSubmit = (data: z.infer<typeof formSchema>) => {
-    console.log(data)
+  const handler = async (formData: {
+    name: string
+    number: string
+    baeName: string
+    baeNumber: string
+  }) => {
+    setLoading(true)
+    const hasSent = await sendData(formData)
+
+    if (hasSent) {
+      form.reset()
+      setLoading(false)
+      setShowDialog(true)
+    }
   }
 
   return (
-    <Form {...form}>
-      <form
-        className="flex flex-col gap-4"
-        onSubmit={form.handleSubmit(onSubmit)}
-      >
-        <FormField
-          name="nome"
-          control={form.control}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel htmlFor="nome">Seu Nome:</FormLabel>
-              <FormControl>
-                <Input {...field} id="nome" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+    <>
+      <Form {...form}>
+        <form
+          className="flex flex-col gap-4"
+          onSubmit={form.handleSubmit(handler)}
+        >
+          <FormField
+            name="name"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="name">Seu Nome:</FormLabel>
+                <FormControl>
+                  <Input {...field} id="name" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          name="telefone"
-          control={form.control}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel htmlFor="telefone">Seu Telefone:</FormLabel>
-              <FormControl>
-                <InputMask mask="(99) \9 9999 9999" {...field}>
-                  <Input id="telefone" />
-                </InputMask>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+          <FormField
+            name="number"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="number">Seu Telefone:</FormLabel>
+                <FormControl>
+                  <InputMask mask="(99) \9 9999 9999" {...field}>
+                    <Input id="number" />
+                  </InputMask>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          name="nomeMozao"
-          control={form.control}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel htmlFor="nomeMozao">Nome do Mozão:</FormLabel>
-              <FormControl>
-                <Input {...field} id="nomeMozao" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+          <FormField
+            name="baeName"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="baeName">Nome do Mozão:</FormLabel>
+                <FormControl>
+                  <Input {...field} id="baeName" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          name="telefoneMozao"
-          control={form.control}
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel htmlFor="telefoneMozao">Telefone do Mozão:</FormLabel>
-              <FormControl>
-                <InputMask mask="(99) \9 9999 9999" {...field}>
-                  <Input id="telefoneMozao" />
-                </InputMask>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+          <FormField
+            name="baeNumber"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="baeNumber">Telefone do Mozão:</FormLabel>
+                <FormControl>
+                  <InputMask mask="(99) \9 9999 9999" {...field}>
+                    <Input id="baeNumber" />
+                  </InputMask>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <Button className="mb-12" type="submit">
-          Enviar
-        </Button>
-      </form>
-    </Form>
+          <Button className="mb-12" type="submit">
+            {loading ? "Enviando..." : "Enviar"}
+          </Button>
+        </form>
+      </Form>
+
+      <AlertDialog open={true}>
+        <AlertDialogContent className="bg-white h-screen flex flex-col justify-center">
+          <AlertDialogHeader>
+            <AlertDialogTitle className="text-color-p">
+              Tudo certinho ❤️!
+            </AlertDialogTitle>
+            <AlertDialogDescription className="text-color-t text-base">
+              Nosso time comercial entrará em contato com vocês em breve.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogAction
+              className="w-28 mx-auto"
+              onClick={() => setShowDialog(false)}
+            >
+              Continuar
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   )
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function obterPrimeiroNome(texto: string) {
+  const palavras = texto.trim().split(/\s+/)
+  return palavras[0]
+}
+
+export function formatarData(date: Date) {
+  const dia = String(date.getDate()).padStart(2, "0")
+  const mes = date.toLocaleString("default", { month: "short" })
+  const ano = date.getFullYear()
+  const horas = String(date.getHours()).padStart(2, "0")
+  const minutos = String(date.getMinutes()).padStart(2, "0")
+
+  return `${dia} ${mes} ${ano} ${horas}:${minutos}`
+}
